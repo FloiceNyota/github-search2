36e22c2d5728a466f328abb7e8aed007cf1e56ac
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-18 11:51:02 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ProfileService } from './services/profile.service'
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    RepositoriesComponent,
+    UserprofileComponent,
+    SearchComponent,
+    NavbarComponent,
     
   ],
   imports: [
