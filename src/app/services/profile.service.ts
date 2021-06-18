3ff65d810150c7a98fd7fc53758021203c31cc4f
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-18 07:58:39 +0300

    add LICENSE

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -12,6 +12,21 @@ export class ProfileService {
   constructor(private http:HttpClient) { 
   
   }
-  
+  getProfileInfo(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  getProfileRepos(){
+  	return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  	.map(res => res.json());
+  }
+
+  updateProfile(username:string){
+  	this.username = username;
+  }
+
+}
+
 
 }
