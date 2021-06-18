8afd447aa36ddf57fbb69c54e21261f88865ab0f
FloiceNyota98 <nyotafloice@gmail.com>
2021-06-18 11:55:45 +0300

    redo components

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { Repo } from '../repo';
+import { UserService } from '../user-service.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
-  styleUrls: ['./repositories.component.css']
+  providers: [UserService],
+  styleUrls: ['./repositories.component.css'],
 })
 export class RepositoriesComponent implements OnInit {
+  Users!: User;
+  Repos: Repo[] = [];
+  constructor(public userHttpService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.searchGit('ngetichnicholas');
   }
-
+  searchGit(searchTerm: string) {
+    this.userHttpService.searchGits(searchTerm).then(
+      (success) => {
+        this.Users = this.userHttpService.Users;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.userHttpService.searchRepos(searchTerm).then((success) => {
+      this.Repos = this.userHttpService.Repos;
+    });
+  }
 }
--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Repo } from '../repo';
+import { UserService } from '../user-service.service';
 
 @Component({
   selector: 'app-userprofile',
   templateUrl: './userprofile.component.html',
-  styleUrls: ['./userprofile.component.css']
+  styleUrls: ['./userprofile.component.css'],
 })
 export class UserprofileComponent implements OnInit {
+  Users!: User;
+  Repos: Repo[] = [];
+  constructor(public userHttpService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.searchGit('ngetichnicholas');
   }
-
+  searchGit(searchTerm: string) {
+    this.userHttpService.searchGits(searchTerm).then(
+      (success) => {
+        this.Users = this.userHttpService.Users;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.userHttpService.searchRepos(searchTerm).then((success) => {
+      this.Repos = this.userHttpService.Repos;
+    });
+  }
 }
